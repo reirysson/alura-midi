b6b1fffc614a894b1bd79574e5a139a6c458138f
reirysson <reiryssonjp@gmail.com>
2022-03-05 18:03:57 -0300

    upload com os sons funcionando para cada função

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-document.querySelector('.tecla_pom');
+function playSound (idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
 
-document.querySelector('#tecla_pom').play();
+const keyList = document.querySelectorAll('.tecla');
+
+let i = 0;
+
+while (i<keyList.length){
+
+    const key = keyList[i];
+    const instrument = key.classList[1];
+
+    const idAudio = `#som_${instrument}`;
+
+    key.onclick = function (){
+        playSound(idAudio);
+    }
+
+    i = i + 1;
+
+    console.log(i);
+}
+
+
